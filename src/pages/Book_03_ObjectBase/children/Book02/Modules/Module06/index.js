c70ecef1bcfff06710887237524a20b9f8ade58c
zxx_ynl <zhangxiaoxi_ynl@163.com>
2023-08-25 09:03:07 +0800

    0825 09:03

--- src/pages/Book_03_ObjectBase/children/Book02/Modules/Module06/index.js
+++ src/pages/Book_03_ObjectBase/children/Book02/Modules/Module06/index.js
@@ -13,7 +13,16 @@ import SmallTitle from '@/components/SmallTitle'
 // 红字wrapper
 // import RedFontWrapper from '@/components/RedFontWrapper'
 import Red from '@/components/RedFontWrapper'
+import Blue from '@/components/BlueFontWrapper'
 
+import Dot from '@/components/Dot'
+
+import img126 from '@/assets/images/img126.png'
+import img127 from '@/assets/images/img127.png'
+import img128 from '@/assets/images/img128.png'
+import img129 from '@/assets/images/img129.png'
+import img130 from '@/assets/images/img130.png'
+import img131 from '@/assets/images/img131.png'
 
 const Module = (props) => {
 
@@ -22,7 +31,89 @@ const Module = (props) => {
             className={styles.base_wrapper}
         >
             <ParagraphWrapper>
-                
+                方法，必须由其所在类或对象调用才有意义。若方法含有参数：
+            </ParagraphWrapper>
+            <div style={{paddingLeft: '12px'}}>
+                <Blue>
+                    <ParagraphWrapper>
+                        形参：方法声明时的参数
+                    </ParagraphWrapper>
+                    <ParagraphWrapper>
+                        实参：方法调用时实际传给形参的参数值
+                    </ParagraphWrapper>
+                </Blue>
+            </div>
+
+            <ComSpace></ComSpace>
+            <ParagraphWrapper>
+                Java的实参值如何传入方法呢？
+            </ParagraphWrapper>
+            <ParagraphWrapper>
+                <Red>Java里方法的参数传递方式只有一种：值传递。 即将实际参数值的副本（复制品）传入方法内，而参数本身不受影响。</Red>
+            </ParagraphWrapper>
+            <ParagraphWrapper>
+                <Blue>形参是<Red>基本数据类型</Red>：将实参基本数据类型变量的“数据值”传递给形参，<Red>所以在方法中操作，不会改变原数据</Red></Blue>
+            </ParagraphWrapper>
+            <ParagraphWrapper>
+                <Blue>形参是<Red>引用数据类型</Red>：将实参引用数据类型变量的“地址值”传递给形参，<Red>所以在方法中操作，会改变原数据</Red>，所以想不影响原数组，得靠拷贝</Blue>
+            </ParagraphWrapper>
+
+            <ComSpace></ComSpace>
+
+            <SmallTitle>关于基本数据类型的demo:</SmallTitle>
+            <ParagraphWrapper>创建一个测试类 testObj。</ParagraphWrapper>
+            <ImagesGroup
+                srcArr={[img126]}
+            ></ImagesGroup>
+
+            <ParagraphWrapper>然后在 main 方法中，定义一个int 数据 a，测试在方法调研前后，及在方法内前后的值。</ParagraphWrapper>
+
+            <ImagesGroup
+                srcArr={[img127]}
+            ></ImagesGroup>
+
+            <ParagraphWrapper>
+                最后运行输出下看看。
+            </ParagraphWrapper>
+
+            <ImagesGroup
+                srcArr={[img128]}
+            ></ImagesGroup>
+
+            <ParagraphWrapper>
+                <Blue>最终外边的 a 的值，没有被改变。</Blue>
+            </ParagraphWrapper>
+
+            <ComSpace></ComSpace>
+
+            <SmallTitle>关于引用数据类型的demo:</SmallTitle>
+
+            <ParagraphWrapper>
+                首先第一一个 Num 类，只用来生成一个实例，可以保存一个 int 值属性
+            </ParagraphWrapper>
+
+            <ImagesGroup
+                srcArr={[img129]}
+            ></ImagesGroup>
+
+            <ParagraphWrapper>
+                然后修改下 testObj 类中的 方法
+            </ParagraphWrapper>
+
+            <ImagesGroup
+                srcArr={[img130]}
+            ></ImagesGroup>
+
+            <ParagraphWrapper>
+                最后在 main 方法中，我们执行下看看
+            </ParagraphWrapper>
+
+            <ImagesGroup
+                srcArr={[img131]}
+            ></ImagesGroup>
+
+            <ParagraphWrapper>
+                <Blue>可以发现，实例对象中的值，在方法执行后，发生了改变</Blue>。
             </ParagraphWrapper>
         </div>
     </>)
