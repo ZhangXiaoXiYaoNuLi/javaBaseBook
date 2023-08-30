66bae7c6e900d9c0e60a1bb15b1ff40bf8635f8d
zxx_ynl <zhangxiaoxi_ynl@163.com>
2023-08-30 14:18:01 +0800

    0830 14:17

--- src/pages/Book_03_ObjectBase/children/Book03/Modules/Module06/index.js
+++ src/pages/Book_03_ObjectBase/children/Book03/Modules/Module06/index.js
@@ -13,7 +13,10 @@ import SmallTitle from '@/components/SmallTitle'
 // 红字wrapper
 // import RedFontWrapper from '@/components/RedFontWrapper'
 import Red from '@/components/RedFontWrapper'
+import Blue from '@/components/BlueFontWrapper'
+import ComLine from '@/components/ComLine'
 
+import Dot from '@/components/Dot'
 
 const Module = (props) => {
 
@@ -21,7 +24,88 @@ const Module = (props) => {
         <div
             className={styles.base_wrapper}
         >
+            <SmallTitle>
+                this是什么？
+            </SmallTitle>
 
+            <ParagraphWrapper>
+                在Java中，this关键字比较难理解，它的作用和其词义很接近。
+            </ParagraphWrapper>
+            <div
+                style={{
+                    paddingLeft: '12px',
+                }}
+            >
+                <ParagraphWrapper>
+                    1、<Blue>它在方法内部使用，即这个方法所属对象的引用</Blue>；
+                </ParagraphWrapper>
+                <ParagraphWrapper>
+                    2、<Blue>它在构造器内部使用，表示该构造器正在初始化的对象</Blue>。
+                </ParagraphWrapper>
+            </div>
+
+            <ParagraphWrapper>
+                <Red>总的来说，this 可以理解为是“当前对象”（动态地指向该类实例化出来的对象）</Red>。
+            </ParagraphWrapper>
+
+            <ComSpace></ComSpace>
+
+            <ParagraphWrapper>
+                <Red>this 可以调用类的属性、方法和构造器</Red>。  
+            </ParagraphWrapper>
+
+            <ParagraphWrapper>
+                什么时候使用this关键字呢？
+            </ParagraphWrapper>
+            <div
+                style={{
+                    paddingLeft: '12px',
+                }}
+            >
+                <ParagraphWrapper>
+                    当在方法内需要用到调用该方法的对象时，就用this。具体的：<Red>我们可以用this来区分属性和局部变量</Red>。
+                </ParagraphWrapper>
+                <ParagraphWrapper>
+                    比如：this.name = name;
+                </ParagraphWrapper>
+                <ParagraphWrapper>
+                    <Red>
+                        也就是在一个类中的各个 setter 属性的 方法中，判断是局部变量还是属性标识符。
+                    </Red>
+                    <Blue>一般来说，这种情况下，this是可以省略的，只有在形参和属性名相同的时候，才必须显式地写上 this </Blue>。
+                </ParagraphWrapper>
+                <ParagraphWrapper>
+                    <Red>
+                        又或者使用“this.方法”来调用对象（类实例化后）的方法。
+                    </Red>
+                </ParagraphWrapper>
+            </div>
+
+            <ComLine></ComLine>
+
+            <SmallTitle>
+                关于 this 调用构造器：
+            </SmallTitle>
+
+            <ParagraphWrapper>
+                1、<Blue>{`可以在类的构造器中使用"this(形参列表)"的方式，调用本类中重载的其他的构造器！`}</Blue>。
+            </ParagraphWrapper>
+
+            <ParagraphWrapper>
+                2、<Blue>明确：构造器中<Red>不能</Red>{`通过"this(形参列表)"的方式调用`}<Red>自身构造器</Red></Blue>。
+            </ParagraphWrapper>
+
+            <ParagraphWrapper>
+                3、{`如果一个类中声明了n个构造器，则最多有 n - 1 个构造器中使用了"this(形参列表)"`}。
+            </ParagraphWrapper>
+
+            <ParagraphWrapper>
+                4、<Blue>{`"this(形参列表)"`}</Blue><Red>{`必须声明在类的构造器的首行！`}</Red>。
+            </ParagraphWrapper>
+
+            <ParagraphWrapper>
+                5、<Blue>在类的<Red>一个构造器</Red>中，<Red>{`最多只能声明一个`}</Red>{`"this(形参列表)"`}</Blue>。
+            </ParagraphWrapper>
         </div>
     </>)
 }
